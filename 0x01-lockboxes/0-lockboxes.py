ce2e30c9d881cf3898c6f7d611f5b9c95a173dfc
deanbirnie <deanicdl@gmail.com>
2023-12-05 19:19:07 +0000

    Task 0 - checking length of list first

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,22 +11,23 @@ def canUnlockAll(boxes):
     unique_keys = [0]
     new_boxes_we_can_open = []
     i = 0
-    if len(boxes) > 0:
-        for box in boxes:
-            if i not in unique_keys:
-                return False
-            for key in box:
-                if key:
-                    if key not in unique_keys and key < len(boxes):
-                        unique_keys.append(key)
-                        new_boxes_we_can_open.append(key)
-            for box2 in new_boxes_we_can_open:
-                if boxes[box2]:
-                    new_keys = boxes[box2]
-                    if new_keys:
-                        for key in new_keys:
-                            if key not in unique_keys:
-                                unique_keys.append(key)
-
-            i += 1
+    if len(boxes) <= 1 or boxes == [[]]:
         return True
+    for box in boxes:
+        if i not in unique_keys:
+            return False
+        for key in box:
+            if key:
+                if key not in unique_keys and key < len(boxes):
+                    unique_keys.append(key)
+                    new_boxes_we_can_open.append(key)
+        for box2 in new_boxes_we_can_open:
+            if boxes[box2]:
+                new_keys = boxes[box2]
+                if new_keys:
+                    for key in new_keys:
+                        if key not in unique_keys:
+                            unique_keys.append(key)
+
+        i += 1
+    return True
