6bf7fab0dbbeb2abdd06e374f76b3bc86030a541
deanbirnie <deanicdl@gmail.com>
2024-01-18 23:00:43 +0200

    Add Star Wars API character retrieval script

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2]
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieId + '/';
+
+request(url, function (error, response, body) {
+    if (error) throw error;
+    const characters = JSON.parse(body).characters;
+
+    for (let i = 0; i < characters.length; i++) {
+        request(characters[i], function (error, response, body) {
+            if (error) throw error;
+            console.log(JSON.parse(body).name);
+        });
+    }
+});
