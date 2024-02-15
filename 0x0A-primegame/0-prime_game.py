b7758192b1538d58d49848a0a4ffeb3f8db487fc
deanbirnie <deanicld@gmail.com>
2024-02-15 17:31:58 +0000

    Task 0 - Original solution to check possible failures of different test cases

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -12,18 +12,10 @@ def isPrime(num):
     if num < 2:
         return False
     else:
-        for i in range(2, int(num ** 0.5) + 1):
+        for i in range(2, int(num/2) + 1):
             if (num % i) == 0:
                 return False
     return True
-
-
-def primeSieve(upperLim):
-    """
-    Helper to pre-calculate all primes up to the max number in an array
-    """
-    primes = [num for num in range(2, upperLim + 1) if isPrime(num)]
-    return primes
 
 
 def isWinner(x, nums):
@@ -31,15 +23,15 @@ def isWinner(x, nums):
     Iterate through nums array to determine who the winner
     of each game will be.
     """
-    if not nums or x < 1 or x > len(nums):
-        return None
-
     maria_wins = 0
     ben_wins = 0
 
     for i in range(x):
-        primes = primeSieve(nums[i])
-        moves = len(primes)
+        moves = 0
+
+        for j in range(1, nums[i] + 1):
+            if isPrime(j):
+                moves += 1
 
         if moves % 2 == 0:
             ben_wins += 1
@@ -50,7 +42,5 @@ def isWinner(x, nums):
         return "Maria"
     elif ben_wins > maria_wins:
         return "Ben"
-    elif maria_wins == ben_wins:
-        return None
     else:
         return None
